136a148147bf33a9d129a46e4803021c1858b8d4
TakaoIsTheBest <saheb1379@gmail.com>
2023-01-01 16:48:22 +0330

    Redo home page and add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
 	});
 
 	return (
-		<>
+		<div className="flex flex-col min-h-screen">
 			<nav
 				className={`fixed w-full flex justify-between items-center p-4 ${
 					showNavbarShadow ? "drop-shadow-md" : ""
@@ -32,18 +32,19 @@ export default function App({ Component, pageProps }: AppProps) {
 					inquire
 				</Link>
 				<div className="flex gap-4 items-center">
-					<Link href="/login" className="font-mono font-bold hover:underline">
+					<Link href="/login" className="font-mono font-bold hover:underline" title="Log in to your account">
 						login
 					</Link>
 					<Link
 						href="/signup"
 						className="solid-box-shadow font-mono font-bold bg-black text-white hover:text-accent p-2"
+						title="Create a new account for free"
 					>
 						SIGN UP FREE
 					</Link>
 				</div>
 			</nav>
 			<Component {...pageProps} />
-		</>
+		</div>
 	);
 }
