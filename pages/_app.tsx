d65bbb90d09350fbdabc6ec82fd4a4febd251918
TakaoIsTheBest <saheb1379@gmail.com>
2023-01-12 12:36:21 +0330

    create different navigation methods for when the user is not logged in and for different screen sizes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 
 // Components
 import MobileNav from "../components/ui/MobileNav";
+import { Button } from "../components/ui/Button";
 
 export default function App({ Component, pageProps }: AppProps) {
 	const [showNavbarShadow, setShowNavbarShadow] = useState(false);
@@ -28,30 +29,64 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<div className="flex flex-col min-h-screen">
 			<nav
-				className={`fixed w-full flex justify-between items-center p-4 ${
+				className={`fixed w-full flex justify-center items-center p-4 ${
 					showNavbarShadow ? "drop-shadow-md" : ""
-				} bg-white md:px-6`}
+				} bg-white md:px-6 md:justify-between`}
 			>
 				<Link href="/" className="solid-text-shadow font-righteous font-bold text-2xl md:text-4xl">
 					inquire
 				</Link>
-				<div className="flex gap-4 items-center">
-					<Link href="/login" className="font-mono font-bold hover:underline" title="Log in to your account">
-						login
-					</Link>
-					<Link
-						href="/signup"
-						className="solid-box-shadow font-mono font-bold bg-black text-white hover:text-accent p-2"
-						title="Create a new account for free"
-					>
-						SIGN UP FREE
-					</Link>
-				</div>
+				{isLoggedIn ? (
+					<div className="hidden gap-4 items-center md:flex">
+						<Link
+							href="/u/name/dashboard"
+							className="font-mono font-bold hover:underline"
+							title="Dashboard page"
+						>
+							dashboard
+						</Link>
+						<Link
+							href="/u/name/settings"
+							className="font-mono font-bold hover:underline"
+							title="Account settings page"
+						>
+							settings
+						</Link>
+						<Button title="Logout">logout</Button>
+					</div>
+				) : (
+					<div className="hidden gap-4 items-center md:flex">
+						<Link
+							href="/login"
+							className="font-mono font-bold hover:underline"
+							title="Log in to your account"
+						>
+							login
+						</Link>
+						<Link
+							href="/signup"
+							className="solid-box-shadow font-mono font-bold bg-black text-white hover:text-accent p-2"
+							title="Create a new account for free"
+						>
+							SIGN UP FREE
+						</Link>
+					</div>
+				)}
 			</nav>
 
 			<Component {...pageProps} />
 
-			{isLoggedIn && <MobileNav />}
+			{isLoggedIn ? (
+				<MobileNav />
+			) : (
+				<Link
+					href="/login"
+					className="flex justify-center items-center bg-accent text-white p-4 font-mono font-bold text-lg hover:underline md:hidden"
+					title="Log in to your account"
+				>
+					Log in to start creating
+				</Link>
+			)}
 
 			<footer className="flex justify-center items-center bg-black text-white p-2">
 				<p className="flex gap-3 text-xs">
