3fafd20a39d745470d9e861a94d257f51ecbab28
TakaoIsTheBest <saheb1379@gmail.com>
2023-01-12 12:21:28 +0330

    made buttons icon-compatable

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -22,7 +22,7 @@ export function Button({
 		<button
 			type={type}
 			onClick={clickHandler}
-			className={`flex-1 font-mono font-bold p-2 ${
+			className={`flex-1 flex justify-center items-center font-mono font-bold p-2 ${
 				color === "primary"
 					? "solid-box-shadow bg-black text-white hover:text-accent disabled:shadow-none disabled:bg-slate-600 disabled:hover:text-white"
 					: "bg-white text-black hover:bg-slate-100 hover:text-accent disabled:bg-slate-100 disabled:text-slate-400 disabled:hover:text-slate-400"
@@ -37,18 +37,20 @@ export function Button({
 type LinkButtonProps = {
 	color?: "primary" | "secondary";
 	href: string;
+	title: string;
 	children?: ReactNode;
 };
 
-export function LinkButton({ color = "primary", href, children }: LinkButtonProps) {
+export function LinkButton({ color = "primary", href, title, children }: LinkButtonProps) {
 	return (
 		<Link
 			href={href}
-			className={`flex-1 flex justify-center font-mono font-bold p-2 ${
+			className={`flex-1 flex justify-center items-center gap-1 font-mono font-bold p-2 ${
 				color === "primary"
 					? "solid-box-shadow bg-black text-white hover:text-accent"
 					: "bg-white text-black hover:bg-slate-100 hover:text-accent"
 			} transition-colors duration-100`}
+			title={title}
 		>
 			{children}
 		</Link>
