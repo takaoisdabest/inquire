588485c792ba51af119a3cda039974673e09e3ef
TakaoIsTheBest <saheb1379@gmail.com>
2023-01-15 19:57:08 +0330

    add danger color and active state for buttons

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import { MouseEventHandler, ReactNode } from "react";
+import { useState } from "react";
 
 type ButtonProps = {
 	type?: "button" | "submit";
-	color?: "primary" | "secondary";
+	color?: "primary" | "secondary" | "danger";
 	clickHandler?: MouseEventHandler<HTMLButtonElement>;
 	isLoading?: boolean;
 	disabled?: boolean;
@@ -26,8 +27,10 @@ export function Button({
 			onClick={clickHandler}
 			className={`flex-1 flex justify-center items-center font-mono font-bold p-2 ${
 				color === "primary"
-					? "solid-box-shadow bg-black text-white hover:text-accent disabled:shadow-none disabled:bg-slate-600 disabled:hover:text-white"
-					: "bg-white text-black hover:bg-slate-100 hover:text-accent disabled:bg-slate-100 disabled:text-slate-400 disabled:hover:text-slate-400"
+					? "solid-box-shadow bg-black text-white hover:text-accent disabled:shadow-none disabled:bg-slate-600 disabled:hover:text-white active:shadow-none"
+					: color === "secondary"
+					? "bg-white text-black hover:bg-slate-100 hover:text-accent disabled:bg-slate-100 disabled:text-slate-400 disabled:hover:text-slate-400 active:bg-slate-200"
+					: "solid-box-shadow-danger bg-black text-white hover:text-red-600 disabled:shadow-none disabled:bg-slate-600 disabled:hover:text-white active:shadow-none"
 			} transition-colors duration-100`}
 			disabled={disabled || isLoading === true}
 			aria-label={ariaLabel}
@@ -38,7 +41,7 @@ export function Button({
 }
 
 type LinkButtonProps = {
-	color?: "primary" | "secondary";
+	color?: "primary" | "secondary" | "danger";
 	href: string;
 	ariaLabel: string;
 	children?: ReactNode;
@@ -50,8 +53,10 @@ export function LinkButton({ color = "primary", href, ariaLabel, children }: Lin
 			href={href}
 			className={`flex-1 flex justify-center items-center gap-1 font-mono font-bold p-2 ${
 				color === "primary"
-					? "solid-box-shadow bg-black text-white hover:text-accent"
-					: "bg-white text-black hover:bg-slate-100 hover:text-accent"
+					? "solid-box-shadow bg-black text-white hover:text-accent active:shadow-none"
+					: color === "secondary"
+					? "bg-white text-black hover:bg-slate-100 hover:text-accent active:bg-slate-200"
+					: "solid-box-shadow-danger bg-black text-white hover:text-red-600 active:shadow-none"
 			} transition-colors duration-100`}
 			aria-label={ariaLabel}
 		>
