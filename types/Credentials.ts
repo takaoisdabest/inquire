e2107c369b95364fe39c311efdec6e46aec75d09
TakaoIsTheBest <saheb1379@gmail.com>
2023-01-21 12:32:38 +0330

    fix sign up type to show error for empty name and confirm password fields

--- types/Credentials.ts
+++ types/Credentials.ts
@@ -11,10 +11,13 @@ export type UserLogin = z.infer<typeof UserLoginSchema>;
 // Schema and type for user sign up credentials
 export const UserSignUpSchema = z
 	.object({
-		name: z.string().max(20, { message: "Name can't be longer than 20 characters" }),
+		name: z
+			.string()
+			.min(1, { message: "Please enter a name" })
+			.max(20, { message: "Name can't be longer than 20 characters" }),
 		email: z.string().email(),
 		password: z.string().min(8, { message: "Password must contain at least 8 characters" }),
-		confirmPassword: z.string()
+		confirmPassword: z.string().min(8, { message: "Your passswords do not match" })
 	})
 	.refine((data) => data.password === data.confirmPassword, {
 		message: "Your passswords do not match",
