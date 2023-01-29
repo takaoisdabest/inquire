4215003e27703fc514f9c152ddf3f3709df1b97f
TakaoIsTheBest <saheb1379@gmail.com>
2023-01-29 19:35:33 +0330

    add sesion provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import NextNProgress from "nextjs-progressbar";
+import { SessionProvider } from "next-auth/react";
 
 // Components
 import MobileNav from "../components/layouts/MobileNav";
@@ -28,80 +29,80 @@ export default function App({ Component, pageProps }: AppProps) {
 	});
 
 	return (
-		<div className="flex flex-col min-h-screen">
-			<nav
-				className={`z-10 fixed w-full flex justify-center items-center p-4 ${
-					showNavbarShadow ? "drop-shadow-md" : ""
-				} bg-white md:px-6 md:justify-between`}
-			>
-				<Link
-					href="/"
-					className="solid-text-shadow font-righteous font-bold text-2xl md:text-4xl"
-					aria-label="home page"
+		<SessionProvider session={pageProps.session}>
+			<div className="flex flex-col min-h-screen">
+				<nav
+					className={`z-10 fixed w-full flex justify-center items-center p-4 ${
+						showNavbarShadow ? "drop-shadow-md" : ""
+					} bg-white md:px-6 md:justify-between`}
 				>
-					inquire
-				</Link>
+					<Link
+						href="/"
+						className="solid-text-shadow font-righteous font-bold text-2xl md:text-4xl"
+						aria-label="home page"
+					>
+						inquire
+					</Link>
+					{isLoggedIn ? (
+						<div className="hidden gap-4 items-center md:flex">
+							<Link
+								href="/u/name/dashboard"
+								className="font-mono font-bold hover:underline"
+								aria-label="dashboard page"
+							>
+								Dashboard
+							</Link>
+							<Link
+								href="/u/name/settings"
+								className="font-mono font-bold hover:underline"
+								aria-label="account settings page"
+							>
+								Settings
+							</Link>
+							<Button ariaLabel="log out">Logout</Button>
+						</div>
+					) : (
+						<div className="hidden gap-4 items-center md:flex">
+							<Link href="/login" className="font-mono font-bold hover:underline" aria-label="log in">
+								login
+							</Link>
+							<Link
+								href="/signup"
+								className="solid-box-shadow font-mono font-bold bg-black text-white hover:text-accent p-2"
+								aria-label="sign up"
+							>
+								SIGN UP FREE
+							</Link>
+						</div>
+					)}
+				</nav>
+
+				<NextNProgress color="#6c63ff" options={{ showSpinner: false }} />
+
+				<Component {...pageProps} />
+
 				{isLoggedIn ? (
-					<div className="hidden gap-4 items-center md:flex">
-						<Link
-							href="/u/name/dashboard"
-							className="font-mono font-bold hover:underline"
-							aria-label="dashboard page"
-						>
-							Dashboard
-						</Link>
-						<Link
-							href="/u/name/settings"
-							className="font-mono font-bold hover:underline"
-							aria-label="account settings page"
-						>
-							Settings
-						</Link>
-						<Button ariaLabel="log out">
-							Logout
-						</Button>
-					</div>
+					<MobileNav />
 				) : (
-					<div className="hidden gap-4 items-center md:flex">
-						<Link href="/login" className="font-mono font-bold hover:underline" aria-label="log in">
-							login
-						</Link>
-						<Link
-							href="/signup"
-							className="solid-box-shadow font-mono font-bold bg-black text-white hover:text-accent p-2"
-							aria-label="sign up"
-						>
-							SIGN UP FREE
-						</Link>
-					</div>
+					<Link
+						href="/login"
+						className="flex justify-center items-center bg-accent text-white p-4 font-mono font-bold text-lg hover:underline md:hidden"
+						aria-label="log in"
+					>
+						Log in to start creating
+					</Link>
 				)}
-			</nav>
 
-			<NextNProgress color="#6c63ff" options={{ showSpinner: false }} />
-
-			<Component {...pageProps} />
-
-			{isLoggedIn ? (
-				<MobileNav />
-			) : (
-				<Link
-					href="/login"
-					className="flex justify-center items-center bg-accent text-white p-4 font-mono font-bold text-lg hover:underline md:hidden"
-					aria-label="log in"
-				>
-					Log in to start creating
-				</Link>
-			)}
-
-			<footer className="flex justify-center items-center bg-black text-white p-2">
-				<p className="flex gap-3 text-xs">
-					<span className="font-righteous text-accent font-bold">inquire</span>
-					<span className="font-extralight">-</span>
-					<span className="font-extralight">Saheb Khadem</span>
-					<span className="font-extralight">-</span>
-					<span className="font-extralight">2023</span>
-				</p>
-			</footer>
-		</div>
+				<footer className="flex justify-center items-center bg-black text-white p-2">
+					<p className="flex gap-3 text-xs">
+						<span className="font-righteous text-accent font-bold">inquire</span>
+						<span className="font-extralight">-</span>
+						<span className="font-extralight">Saheb Khadem</span>
+						<span className="font-extralight">-</span>
+						<span className="font-extralight">2023</span>
+					</p>
+				</footer>
+			</div>
+		</SessionProvider>
 	);
 }
